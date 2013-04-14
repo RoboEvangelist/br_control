225354928c4c5955aa48ed95864e1fac12be5267
Pototo <pototobill@gmail.com>
2013-04-14 03:02:30 -0700

    separating the rover class into separate classes

--- br_control.py
+++ br_control.py
@@ -24,7 +24,9 @@ class RovCon():
 		self.moveSocket.setblocking(1)
 		
 		# set up rover for communication
-		msg = 'GET /check_user.cgi?user=AC13&pwd=AC13 HTTP/1.1\r\nHost: 192.168.1.100:80\r\nUser-Agent: WifiCar/1.0 CFNetwork/485.12.7 Darwin/10.4.0\r\nAccept: */*\r\nAccept-Language: en-us\r\nAccept-Encoding: gzip, deflate\r\nConnection: keep-alive\r\n\r\n'
+		msg = 'GET /check_user.cgi?user=AC13&pwd=AC13 HTTP/1.1\r\nHost: 192.168.1.100:80\r\nUser-Agent: WifiCar/1.0 \
+		CFNetwork/485.12.7 Darwin/10.4.0\r\nAccept: */*\r\nAccept-Language: en-us\r\nAccept-Encoding: gzip, deflate\r\n \
+		Connection: keep-alive\r\n\r\n'
 		self.moveSocket.send(msg)
 
 		# Get the return message
@@ -118,35 +120,6 @@ class RovCon():
 		while len(data) == 0:
 			data = self.moveSocket.recv(self.maxTCPBuffer)
 		#print list(data)
-
-		# Create new socket for video
-		self.videoSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.videoSocket.connect((self.host,self.port))
-		self.videoSocket.setblocking(1)
-
-		mc = array.array('c')
-		mc.extend(['M','O','_','V']);
-		mc.extend('\0')
-		i = 0
-		while i < 10:
-			mc.extend('\0')
-			i = i + 1
-		mc.extend('\x04')
-		i = 0
-		while i < 3:
-			mc.extend('\0')
-			i = i + 1
-		mc.extend('\x04')
-		i = 0
-		while i < 3:
-			mc.extend('\0')
-			i = i + 1
-		ldata = list(data)
-		id_cp = ldata[25:29]
-		mc.extend(id_cp)
-		#print mc, identifier of the image(?)
-		msg = mc.tostring()
-		self.videoSocket.send(msg)
 
 	def disconnectRover(self):
 		self.moveSocket.close()
