f0f47f8bebb0b5dba7d8d47398d2f9437da98dfa
Pototo <pototobill@gmail.com>
2013-04-23 15:14:12 -0700

    updated files in the script folder

--- scripts/br_cam.py
+++ scripts/br_cam.py
@@ -6,73 +6,114 @@ from std_msgs.msg import String
 import cv2
 
 import socket
-import time
-import datetime
 import array
-import struct
 
 class RovCam(): 
-	def __init__(self, data):
-		self.host = '192.168.1.100'
-		self.port = 80
-		self.maxTCPBuffer = 2048
-		self.initConnection(data)     #image id is taken from data 
+    def __init__(self, data):
+        self.host = '192.168.1.100'
+        self.port = 80
+        self.max_tcp_cmd_buffer = 2048
+        self.init_connection(data)     #image id is taken from data 
 
-	def initConnection(self, data):
-		# set up rover for communication
-		msg = 'GET /check_user.cgi?user=AC13&pwd=AC13 HTTP/1.1\r\nHost: 192.168.1.100:80\r\nUser-Agent: WifiCar/1.0 \
-		CFNetwork/485.12.7 Darwin/10.4.0\r\nAccept: */*\r\nAccept-Language: en-us\r\nAccept-Encoding: gzip, deflate\r\n \
-		Connection: keep-alive\r\n\r\n'
+    def init_connection(self, data):
+	# set up rover for communication
+        # append instead of writting everything in one line
+        msg = ['GET /check_user.cgi?user=AC13&pwd=AC13 HTTP/1.1\r\nHost: ']
+        msg.append('192.168.1.100:80\r\n')
+        msg.append('User-Agent: WifiCar/1.0 CFNetwork/485.12.7 ')
+        msg.append('Darwin/10.4.0\r\nAccept: */*\r\nAccept-Language: ')
+        msg.append('en-us\r\nAccept-Encoding: gzip, deflate\r\n')
+        msg.append('Connection: keep-alive\r\n\r\n')
+        msg = ''.join(msg)
 
-		# Create new socket for video
-		self.connectRover()
+	# Create new socket for video
+        self.connect_rover()
 
-		mc = array.array('c')
-		mc.extend(['M','O','_','V']);
-		mc.extend('\0')
-		i = 0
-		while i < 10:
-			mc.extend('\0')
-			i = i + 1
-		mc.extend('\x04')
-		i = 0
-		while i < 3:
-			mc.extend('\0')
-			i = i + 1
-		mc.extend('\x04')
-		i = 0
-		while i < 3:
-			mc.extend('\0')
-			i = i + 1
-		ldata = list(data)
-		id_cp = ldata[25:29]
-		mc.extend(id_cp)
-		#print mc, identifier of the image(?)
-		msg = mc.tostring()
-		self.videoSocket.send(msg)
+        m_c = array.array('c')
+        m_c.extend(['M', 'O', '_', 'V'])
+        m_c.extend('\0')
+        i = 0
+        while i < 10:
+            m_c.extend('\0')
+            i = i + 1
+        m_c.extend('\x04')
+        i = 0
+        while i < 3:
+            m_c.extend('\0')
+            i = i + 1
+        m_c.extend('\x04')
+        i = 0
+        while i < 3:
+            m_c.extend('\0')
+            i = i + 1
+        ldata = list(data)
+        id_cp = ldata[25:29]
+        m_c.extend(id_cp)
+        #print m_c, identifier of the image(?)
+        msg = m_c.tostring()
+        self.video_socket.send(msg)
 
-	def connectRover(self):	
-		self.videoSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.videoSocket.connect((self.host,self.port))
-		self.videoSocket.setblocking(1)
+    def connect_rover(self):	
+        self.video_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.video_socket.connect((self.host, self.port))
+        self.video_socket.setblocking(1)
 
-	def disconnectVideo(self):
-		self.videoSocket.close()
+    def disconnect_video(self):
+        self.video_socket.close()
 
-	def writeCmd(self, index, extraInput):	
+    def write_cmd(self, index, extra_input):	
 #	    Robot's Control Packets
-		len = 26                          # actuall length of the video buffer
-		buffer = array.array('c')
-		buffer.extend(['M','O','_','V']);
-		for i in range(4,len+1):	
-			buffer.append('\0')
-		buffer[15] = '\x04'
-		buffer[19] = '\x04'
-		for i in range(0,3):
-			if (len(extraInput) >= 4):
-				buffer[i + 22] = extraInput[i]
-			else:	
-				buffer[i + 22] = '\0'     #extraInput[1]
+        packet_len = 26                          # actuall packet_length of the video cmd_buffer
+        cmd_buffer = array.array('c')
+        cmd_buffer.extend(['M', 'O', '_', 'V'])
+        for i in range(4, packet_len+1):	
+            cmd_buffer.append('\0')
+        cmd_buffer[15] = '\x04'
+        cmd_buffer[19] = '\x04'
+        for i in range(0, 3):
+            if (len(extra_input) >= 4):
+                cmd_buffer[i + 22] = extra_input[i]
+            else:	
+                cmd_buffer[i + 22] = '\0'     #extra_input[1]
 		
-		msg = buffer.tostring()
-		self.videoSocket.send(msg) 
+        msg = cmd_buffer.tostring()
+        self.video_socket.send(msg) 
+
+    def display_image(self):
+ 	# For now just get one frame, we have to make this a loop of course
+        print 'Get video frame!'
+        data = ''
+        ldata = []
+        start = ''
+        while len(data) == 0:
+            data = self.video_socket.recv(self.max_tcp_cmd_buffer)
+            list_data = list(data)
+            m_c = array.array('c')
+            m_c.extend (list_data[0:4])
+
+            if (start == ''):
+                start = 'first'
+            else:
+                start = m_c.tostring()
+            if (start == 'MO_V'):
+                break
+            else:
+                ldata.extend(list_data)
+
+            data = ''
+
+		# Write image to "test.jpg"
+            img = ldata[36:]
+        jpgfile = open('test.jpg', 'wb')
+        for i in img:
+            jpgfile.write(i)
+		# Close file handlers
+        jpgfile.close()
+
+        image = cv2.imread('test.jpg')
+        #cv2.NamedWindow('hola', WINDOW_AUTOSIZE)
+        #cv2.ShowImage('robot window', image)
+        cv2.imshow('hola', image)
+        cv2.waitKey(100)
+        cv2.destroyWindow('test.jpg')
+
--- scripts/br_control.py
+++ scripts/br_control.py
@@ -7,130 +7,132 @@ import br_cam
 
 import socket
 import time
-import datetime
 import array
-import struct
 
 class RovCon(): 
-	def __init__(self):
-		self.host = '192.168.1.100'
-		self.port = 80
-		self.maxTCPBuffer = 2048
-		self.initConnection()
-		self.data = ''
+    def __init__(self):
+        self.host = '192.168.1.100'
+        self.port = 80
+        self.max_tcp_cmd_buffer = 2048
+        self.init_connection()
+        self.final_data = ''
 
-	def initConnection(self):
-		self.connectRover()
+    def init_connection(self):
+        self.connect_rover()
 
-		# set up rover for communication
-		msg = 'GET /check_user.cgi?user=AC13&pwd=AC13 HTTP/1.1\r\nHost: \
-		192.168.1.100:80\r\nUser-Agent: WifiCar/1.0 CFNetwork/485.12.7 \
-		Darwin/10.4.0\r\nAccept: */*\r\nAccept-Language: \
-		en-us\r\nAccept-Encoding: gzip, deflate\r\n Connection: keep-alive\r\n\r\n'
-		self.moveSocket.send(msg)
+	# set up rover for communication
+        msg = ['GET /check_user.cgi?user=AC13&pwd=AC13 HTTP/1.1\r\nHost: '] 
+        msg.append('192.168.1.100:80\r\n')
+        msg.append('User-Agent: WifiCar/1.0 CFNetwork/485.12.7 ')
+        msg.append('Darwin/10.4.0\r\nAccept: */*\r\nAccept-Language: ')
+        msg.append('en-us\r\nAccept-Encoding: gzip, deflate\r\n')
+        msg.append('Connection: keep-alive\r\n\r\n')
+        msg = ''.join(msg)
+
+        self.move_socket.send(msg)
 
 		# Get the return message
-		print 'Wait for HTML return msg'
-		data = ''
-		while len(data) == 0:
-			data = self.moveSocket.recv(self.maxTCPBuffer)
-		print data
+        print 'Wait for HTML return msg'
+        data = ''
+        while len(data) == 0:
+            data = self.move_socket.recv(self.max_tcp_cmd_buffer)
+        print data
 
 		# We have to close the socket and open it again
-		self.disconnnectRover()
-		self.connectRover()
+        self.disconnect_rover()
+        self.connect_rover()
 
 		# The first MO_O command
-		mc = array.array('c')
-		mc.extend(['M','O','_','O']);
-		i = 0
-		mc.extend('\x00')
-		while i < 18:
-			mc.extend('\0')
-			i = i + 1
-		msg = mc.tostring()
-		self.moveSocket.send(msg)
+        m_c = array.array('c')
+        m_c.extend(['M', 'O', '_', 'O'])
+        i = 0
+        m_c.extend('\x00')
+        while i < 18:
+            m_c.extend('\0')
+            i = i + 1
+        msg = m_c.tostring()
+        self.move_socket.send(msg)
 
-		print 'Wait for result on 1st MO command'
-		data = ''
-		while len(data) == 0:
-			data = self.moveSocket.recv(self.maxTCPBuffer)
-		ldata = list(data)
-		msg_i = ldata[4]
+        print 'Wait for result on 1st MO command'
+        data = ''
+        while len(data) == 0:
+            data = self.move_socket.recv(self.max_tcp_cmd_buffer)
+        #ldata = list(data)
+        # msg_i = ldata[4]
 
 		# The second MO_O command
 		#49=4+1+10+1+7+4+9+4+9
-		mc = array.array('c')
-		mc.extend(['M','O','_','O']);
-		mc.extend('\x02')
-		i = 0
-		while i < 10:
-			mc.extend('\0')
-			i = i + 1
-		mc.extend('\x1a')
-		i = 0
-		while i < 7:
-			mc.extend('\0')
-			i = i + 1
-		mc.extend(['A','C','1','3']);
-		i = 0
-		while i < 9:
-			mc.extend('\0')
-			i = i + 1
-		mc.extend(['A','C','1','3']);
-		i = 0
-		while i < 9:
-			mc.extend('\0')
-			i = i + 1
-		msg = mc.tostring()
-		self.moveSocket.send(msg)
+        m_c = array.array('c')
+        m_c.extend(['M', 'O', '_', 'O'])
+        m_c.extend('\x02')
+        i = 0
+        while i < 10:
+            m_c.extend('\0')
+            i = i + 1
+        m_c.extend('\x1a')
+        i = 0
+        while i < 7:
+            m_c.extend('\0')
+            i = i + 1
+        m_c.extend(['A', 'C', '1', '3'])
+        i = 0
+        while i < 9:
+            m_c.extend('\0')
+            i = i + 1
+        m_c.extend(['A', 'C', '1', '3'])
+        i = 0
+        while i < 9:
+            m_c.extend('\0')
+            i = i + 1
+        msg = m_c.tostring()
+        self.move_socket.send(msg)
 
-		print 'Wait for next MO msg'
-		data = ''
-		while len(data) == 0:
-			data = self.moveSocket.recv(self.maxTCPBuffer)
+        print 'Wait for next MO msg'
+        data = ''
+        while len(data) == 0:
+            data = self.move_socket.recv(self.max_tcp_cmd_buffer)
 		#print list(data)
 
-		mc = array.array('c')
-		mc.extend(['M','O','_','O']);
-		mc.extend('\x04')
-		i = 0
-		while i < 10:
-			mc.extend('\0')
-			i = i + 1
-		mc.extend('\x01')
-		i = 0
-		while i < 3:
-			mc.extend('\0')
-			i = i + 1
-		mc.extend('\x01')
-		i = 0
-		while i < 3:
-			mc.extend('\0')
-			i = i + 1
-		mc.extend('\x02')
-		msg = mc.tostring()
-		self.moveSocket.send(msg)
+        m_c = array.array('c')
+        m_c.extend(['M', 'O', '_', 'O'])
+        m_c.extend('\x04')
+        i = 0
+        while i < 10:
+            m_c.extend('\0')
+            i = i + 1
+        m_c.extend('\x01')
+        i = 0
+        while i < 3:
+            m_c.extend('\0')
+            i = i + 1
+            m_c.extend('\x01')
+        i = 0
+        while i < 3:
+            m_c.extend('\0')
+            i = i + 1
+        m_c.extend('\x02')
+        msg = m_c.tostring()
+        self.move_socket.send(msg)
 
-		print 'Wait for next MO msg'
-		data = ''
-		while len(data) == 0:
-			data = self.moveSocket.recv(self.maxTCPBuffer)
+        print 'Wait for next MO msg'
+        data = ''
+        while len(data) == 0:
+            data = self.move_socket.recv(self.max_tcp_cmd_buffer)
 		#print list(data)
-		self.data = data
+        self.final_data = data
 
-	def connectRover(self):	
-		self.moveSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.moveSocket.connect((self.host,self.port))
-		self.moveSocket.setblocking(1)
+    def connect_rover(self):	
+        self.move_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.move_socket.connect((self.host, self.port))
+        self.move_socket.setblocking(1)
 
-	def disconnectRover(self):
-		self.moveSocket.close()
+    def disconnect_rover(self):
+        self.move_socket.close()
 
-	def returnData(self):
-		return self.data
+    def return_data(self):
+        return self.final_data
 
-	def writeCmd(self, index, extraInput):	
+    def write_cmd(self, index, extra_input):	
 #	     Robot's Control Packets
 
 # 		     The left brake command is 
@@ -160,205 +162,151 @@ class RovCon():
 # 		      0010 00 00 00 01 00 00 00 02 0a
  
 		# index is integer which specifies which command to send
-		#extraInput is a javaArray of Bytes
+		#extra_input is a javaArray of Bytes
 		
-		len = 0
-		if index == 1:
-			len = 22
-		elif index == 2:
-			len = 48
-		elif index == 3:
-			len = 23
-		elif index == 4: 
-			len = 26 
-		elif index == 5:
-			len = 24
-		elif index == 6:
-			len = 24
-		elif index == 7:
-			len = 24
-		elif index == 8:
-			len = 24
-		elif index == 9:
-			len = 22
-		elif index == 10:
-			len = 23
-		elif index == 11:
-			len = 23
-		elif index == 12:
-			len = 24
-		elif index == 13:
-			len = 24
+        packet_len = 0
+        if index == 1:
+            packet_len = 22
+        elif index == 2:
+            packet_len = 48
+        elif index == 3:
+            packet_len = 23
+        elif index == 5:
+            packet_len = 24
+        elif index == 6:
+            packet_len = 24
+        elif index == 7:
+            packet_len = 24
+        elif index == 8:
+            packet_len = 24
+        elif index == 9:
+            packet_len = 22
+        elif index == 10:
+            packet_len = 23
+        elif index == 11:
+            packet_len = 23
+        elif index == 12:
+            packet_len = 24
+        elif index == 13:
+            packet_len = 24
 
-		buffer = array.array('c')
-		buffer.extend(['M','O','_','O']);
-		if index == 4:
-			buffer[3] = 'V'
-		for i in range(4,len+1):	
-			buffer.append('\0')
+        cmd_buffer = array.array('c')
+        cmd_buffer.extend(['M', 'O', '_', 'O'])
+        for i in range(4, packet_len+1):	
+            cmd_buffer.append('\0')
 
-		if index == 1:
-			buffer[4] = '\x02'
-		elif index == 2:
-			buffer[4] = '\x02'
-			buffer[15] = '\x1a'
-			buffer[23] = 'A'
-			buffer[24] = 'C'
-			buffer[25] = '1'
-			buffer[26] = '3'
-			buffer[36] = 'A'
-			buffer[37] = 'C'
-			buffer[38] = '1'
-			buffer[39] = '3'
-		elif index == 3:
-			buffer[4] = '\x04'
-			buffer[15] = '\x01'
-			buffer[19] = '\x01'
-			buffer[23] = '\x02'
-		elif index == 4: 
-			buffer[15] = '\x04'
-			buffer[19] = '\x04'
-			for i in range(0,3):
-				if (len(extraInput) >= 4):
-					buffer[i + 22] = extraInput[i]
-				else:	
-					buffer[i + 22] = '\0'     #extraInput[1]
-		elif index == 5:     # left wheel Forward
-			buffer[4] = '\xfa'
-			buffer[15] = '\x02'
-			buffer[19] = '\x01'
-			buffer[23] = '\x04'
-			buffer[24] = '\x0a'
-		elif index == 6:    # left wheel Backward
-			buffer[4] = '\xfa'
-			buffer[15] = '\x02'
-			buffer[19] = '\x01'
-			buffer[23] = '\x05'
-			buffer[24] = '\x0a'
-		elif index == 7:    # right wheel Forward
-			buffer[4] = '\xfa'
-			buffer[15] = '\x02'
-			buffer[19] = '\x01'
-			buffer[23] = '\x01'
-			buffer[24] = '\x0a'
-		elif index == 8:    # right whell backward
-			buffer[4] = '\xfa'
-			buffer[15] = '\x02'
-			buffer[19] = '\x01'
-			buffer[23] = '\x02'
-			buffer[24] = '\x0a'
-		elif index == 9:    # IR off(?)
-			buffer[4] = '\xff'
-		elif index == 10:   # switches infrared LED on
-			buffer[4] = '\x0e'
-			buffer[15] = '\x01'
-			buffer[19] = '\x01'
-			buffer[23] = '\x5e'
-		elif index == 11:   # switches infrared LED off
-			buffer[4] = '\x0e'
-			buffer[15] = '\x01'
-			buffer[19] = '\x01'
-			buffer[23] = '\x5f'
-		elif index == 12:   # stop left track
-			buffer[4] = '\xfa'
-			buffer[15] = '\x02'
-			buffer[19] = '\x01'
-			buffer[23] = '\x02'
-			buffer[24] = '\x00'
-		elif index == 13:  # stop right track
-			buffer[4] = '\xfa'
-			buffer[15] = '\x02'
-			buffer[19] = '\x01'
-			buffer[23] = '\x04'
-			buffer[24] = '\x00'
-		msg = buffer.tostring()
-		if index == 4:
-			self.videoSocket.send(msg)   				
-		else:
-			self.moveSocket.send(msg)
+        if index == 1:
+            cmd_buffer[4] = '\x02'
+        elif index == 2:
+            cmd_buffer[4] = '\x02'
+            cmd_buffer[15] = '\x1a'
+            cmd_buffer[23] = 'A'
+            cmd_buffer[24] = 'C'
+            cmd_buffer[25] = '1'
+            cmd_buffer[26] = '3'
+            cmd_buffer[36] = 'A'
+            cmd_buffer[37] = 'C'
+            cmd_buffer[38] = '1'
+            cmd_buffer[39] = '3'
+        elif index == 3:
+            cmd_buffer[4] = '\x04'
+            cmd_buffer[15] = '\x01'
+            cmd_buffer[19] = '\x01'
+            cmd_buffer[23] = '\x02'
+        elif index == 5:     # left wheel Forward
+            cmd_buffer[4] = '\xfa'
+            cmd_buffer[15] = '\x02'
+            cmd_buffer[19] = '\x01'
+            cmd_buffer[23] = '\x04'
+            cmd_buffer[24] = '\x0a'
+        elif index == 6:    # left wheel Backward
+            cmd_buffer[4] = '\xfa'
+            cmd_buffer[15] = '\x02'
+            cmd_buffer[19] = '\x01'
+            cmd_buffer[23] = '\x05'
+            cmd_buffer[24] = '\x0a'
+        elif index == 7:    # right wheel Forward
+            cmd_buffer[4] = '\xfa'
+            cmd_buffer[15] = '\x02'
+            cmd_buffer[19] = '\x01'
+            cmd_buffer[23] = '\x01'
+            cmd_buffer[24] = '\x0a'
+        elif index == 8:    # right whell backward
+            cmd_buffer[4] = '\xfa'
+            cmd_buffer[15] = '\x02'
+            cmd_buffer[19] = '\x01'
+            cmd_buffer[23] = '\x02'
+            cmd_buffer[24] = '\x0a'
+        elif index == 9:    # IR off(?)
+            cmd_buffer[4] = '\xff'
+        elif index == 10:   # switches infrared LED on
+            cmd_buffer[4] = '\x0e'
+            cmd_buffer[15] = '\x01'
+            cmd_buffer[19] = '\x01'
+            cmd_buffer[23] = '\x5e'
+        elif index == 11:   # switches infrared LED off
+            cmd_buffer[4] = '\x0e'
+            cmd_buffer[15] = '\x01'
+            cmd_buffer[19] = '\x01'
+            cmd_buffer[23] = '\x5f'
+        elif index == 12:   # stop left track
+            cmd_buffer[4] = '\xfa'
+            cmd_buffer[15] = '\x02'
+            cmd_buffer[19] = '\x01'
+            cmd_buffer[23] = '\x02'
+            cmd_buffer[24] = '\x00'
+        elif index == 13:  # stop right track
+            cmd_buffer[4] = '\xfa'
+            cmd_buffer[15] = '\x02'
+            cmd_buffer[19] = '\x01'
+            cmd_buffer[23] = '\x04'
+            cmd_buffer[24] = '\x00'
+        msg = cmd_buffer.tostring()
+        self.move_socket.send(msg)
 
-# robot's speed is ~2 feet/second
+    # robot's speed is ~2 feet/second
 
-	def moveForward(self, distance, speed):
-		speed = 2
-		moveTime = distance/speed
-		iniTime = time.time()
-		deltaTime = 0
-		while deltaTime <= moveTime:
-			self.writeCmd(7,0)
-			self.writeCmd(5,0)
-			deltaTime = time.time() - iniTime
+    def move_forward(self, distance, speed):
+        speed = 2
+        move_time = distance/speed
+        init_time = time.time()
+        delta_time = 0
+        while delta_time <= move_time:
+            self.write_cmd(7, 0)
+            self.write_cmd(5, 0)
+            delta_time = time.time() - init_time
 		# stop tracks
-		self.writeCmd(12,0)
-		self.writeCmd(13,0)
+        self.write_cmd(12, 0)
+        self.write_cmd(13, 0)
 
-	def moveLeftForward(self, distance, speed):
-		speed = 1
-		moveTime = distance/speed
-		iniTime = time.time()
-		deltaTime = 0
-	#	while deltaTime <= moveTime:       
-		self.writeCmd(7,0)
-	#		deltaTime = datetime.datetime.now() - deltaTime  
+    def move_left_forward(self, distance, speed):
+        speed = 1
+        move_time = distance/speed
+        init_time = time.time()
+        delta_time = 0
+    #	while delta_time <= move_time:       
+        self.write_cmd(7, 0)
+    #		delta_time = datetime.datetime.now() - delta_time  
 		# stop tracks
-		self.writeCmd(13,0)
-
-	def displayImage(self):
- 		# For now just get one frame, we have to make this a loop of course
-		print 'Get video frame!'
-		data = ''
-		ldata = []
-		start = ''
-		while len(data) == 0:
-			data = self.videoSocket.recv(self.maxTCPBuffer)
-			ld = list(data)
-			mc = array.array('c')
-			mc.extend (ld[0:4])
-
-			if (start == ''):
-				start = 'first'
-			else:
-				start = mc.tostring()
-			if (start == 'MO_V'):
-				break
-			else:
-				ldata.extend(ld)
-
-			data = ''
-
-		# Write image to "test.jpg"
-		img = ldata[36:]
-		jpgfile = open('test.jpg','wb')
-		for i in img:
-			jpgfile.write(i)
-		# Close file handlers
-		jpgfile.close()
-
-		image = cv2.imread('test.jpg')
-		#cv2.NamedWindow('hola', WINDOW_AUTOSIZE)
-		#cv2.ShowImage('robot window', image)
-		cv2.imshow('hola', image)
-		cv2.waitKey(100)
-		cv2.destroyWindow('test.jpg')
+        self.write_cmd(13, 0)
 
 if __name__ == '__main__':
     try:
-	pub = rospy.Publisher('chatter', String)
-	rospy.init_node('AC13_robot')
-	rover = RovCon() 
-	video = RovCam(rover.returnData())
-	counter = 0
-	distance = 0.5    # feet
-	speed = 1         # foot/sec
-	while not rospy.is_shutdown(): 
-		str = "robot moves %s" % rospy.get_time()
-		rospy.loginfo(str)
-		pub.publish(String(str))
-#		rover.moveForward(distance,speed)
-		rover.displayImage()
-		rospy.sleep(0.1)
-#		counter = counter + 1
+        pub = rospy.Publisher('chatter', String)
+        rospy.init_node('AC13_robot')
+        rover = RovCon() 
+        rover_video = br_cam.RovCam(rover.return_data())
+        distance = 0.5    # feet
+        speed = 1         # foot/sec
+        while not rospy.is_shutdown(): 
+            str = "robot moves %s" % rospy.get_time()
+            rospy.loginfo(str)
+            pub.publish(String(str))
+            rover.move_forward(distance, speed)
+            #rover_video.display_image()
+            rospy.sleep(0.1)
+#           counter = counter + 1
 
-	rover.disconnectRover()
+        rover.disconnect_rover()
     except rospy.ROSInterruptException:
         pass
