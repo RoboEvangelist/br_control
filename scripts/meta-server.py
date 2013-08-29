29e0df66420270e897b3371310ea46a44cbd11d4
Pototo <pototo@pototo-G46VW.(none)>
2013-08-29 01:01:04 -0700

    putting roscore call and br program in separate threads instead of running everything at one

--- scripts/meta-server.py
+++ scripts/meta-server.py
@@ -2,18 +2,42 @@
 from SimpleXMLRPCServer import SimpleXMLRPCServer
 import xmlrpclib
 import subprocess
+from threading import Thread
 
 
 START_ROS_ROVER = []    # stores roscor and rover program
 
 
+class myThread (Thread):
+    def __init__(self, cmd):
+        Thread.__init__(self)
+        self.cmd = cmd
+        self.cmd_process = []
+    def run(self):
+        self.cmd_process.append(subprocess.Popen(self.cmd))
+
 def getServerAddress():
     # commands to start roscore and the rovers ROS program
     roscore_cmd = ['roscore']
-    br_cmd = ['rosrun', 'br_control.py']
-    START_ROS_ROVER.append(subprocess.Popen(roscore_cmd))
-
-
+    br_cmd = ['rosrun', 'br_swarm_rover', 'br_control.py']
+    roscore_thread = myThread(roscore_cmd)
+    roscore_thread.start()
+    rover_started = True    # true if rover program started
+    while rover_started:
+        try:
+            print('before trying rover')
+            rover_thread = myThread(br_cmd)
+            rover_thread.start()
+            rover_started = False
+            print('after trying Rover program')
+        except BaseException:
+            print('trying to connect to rover(s)')
+            pass
 
 if __name__ == '__main__':
-    getServerAddress() 
+    try:
+        getServerAddress() 
+    except BaseException:
+        print('exiting ROS program')
+        from sys import exit
+        exit()
