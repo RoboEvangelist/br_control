92b68982cae0e6b9500a0ceab6171e0cdd0b9cb2
Pototo <pototo@pototo-G46VW.(none)>
2013-08-29 01:10:52 -0700

    adding delays to allow rover software to run after roscore

--- scripts/meta-server.py
+++ scripts/meta-server.py
@@ -3,6 +3,7 @@ from SimpleXMLRPCServer import SimpleXMLRPCServer
 import xmlrpclib
 import subprocess
 from threading import Thread
+from time import sleep
 
 
 START_ROS_ROVER = []    # stores roscor and rover program
@@ -18,26 +19,32 @@ class myThread (Thread):
 
 def getServerAddress():
     # commands to start roscore and the rovers ROS program
+    threads = []                # stores active threads
     roscore_cmd = ['roscore']
     br_cmd = ['rosrun', 'br_swarm_rover', 'br_control.py']
     roscore_thread = myThread(roscore_cmd)
     roscore_thread.start()
-    rover_started = True    # true if rover program started
-    while rover_started:
+    threads.append(roscore_thread)
+    rover_started = False    # true if rover program started
+    sleep(3)
+    while not rover_started:
         try:
-            print('before trying rover')
             rover_thread = myThread(br_cmd)
             rover_thread.start()
-            rover_started = False
-            print('after trying Rover program')
+            rover_started = True
+            threads.append(rover_thread)
         except BaseException:
             print('trying to connect to rover(s)')
             pass
+    return threads
 
 if __name__ == '__main__':
+    threads = []
     try:
-        getServerAddress() 
+        threads = getServerAddress() 
     except BaseException:
         print('exiting ROS program')
+        for thread in threads:
+            thread.join(2.0)
         from sys import exit
         exit()
